942ce346161f5aace1160a0f6e07fc8b108248ef
Rodrigo Gouvêa <rodrigosggouvea@gmail.com>
2011-02-01 15:18:34 -0200

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReicerLeague::Application.config.secret_token = '91e917519d08317ade566d925373ef3daaf3ec73620754cca868c455f54b219831b02159a28c96f3e7cbf50fc045feae450c02eec7c19929d7ce1b9d7cc2e937'
